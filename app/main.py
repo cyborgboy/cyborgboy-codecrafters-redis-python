80a60c87b4a513cd97d8874e3824c524f54a8007
cyborgboy <18073052+cyborgboy@users.noreply.github.com>
2024-11-23 21:01:42 +0530

    typo error

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket  # noqa: F401
 def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _= server_scoket.accept()
+    connection, _= server_socket.accept()
     connection.sendall(b"+PONG\r\n")
 
 
