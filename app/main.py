2cbe5c1ec869ab7b5a60b5355eb607cd85c33865
cyborgboy <18073052+cyborgboy@users.noreply.github.com>
2024-11-23 22:09:58 +0530

    increase buffer size

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _= server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    while connection.recv(1024):
+        connection.sendall(b"+PONG\r\n")
+    
 
 
 
