e3a93dcce08a81efcd92da28b7a32d7ae4d0679a
cyborgboy <18073052+cyborgboy@users.noreply.github.com>
2024-12-01 14:14:25 +0530

    changes added to threading

--- app/main.py
+++ app/main.py
@@ -7,14 +7,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _= server_socket.accept()
-    thread = threading.Thread(target=connect, args=[connection,address])
-    thread.start()
     while connection.recv(1024):
         connection.sendall(b"+PONG\r\n")
+        thread = threading.Thread(target=connect, args=[connection,address])
+        thread.start()
 
 def connect(connect: socket.socket):
 
 
 if __name__ == "__main__":
-    main()
- 2 vb 7
+    main() 2 vb 7
